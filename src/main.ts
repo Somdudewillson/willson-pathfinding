fd0b01f2c4cf9364d9e80efb4c3cb0bc23779bef
somdudewillson <somdudewilson@gmail.com>
2022-03-19 16:01:29 -0700

    Added callbacks.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import { Pathfinder } from "./pathfinding/pathfinder";
+import { testDroneUpdate, TESTDRONE_ENTITYTYPE } from "./testEntity/testDrone";
+
 const MOD_NAME = "willson-pathfinding";
 
 export function main(): void {
@@ -6,12 +9,13 @@ export function main(): void {
   const mod = RegisterMod(MOD_NAME, 1);
 
   // Set a callback function that corresponds to when a new run is started
-  mod.AddCallback(ModCallbacks.MC_POST_GAME_STARTED, postGameStarted);
+  mod.AddCallback(ModCallbacks.MC_POST_NEW_ROOM, Pathfinder.updateRoom);
+  mod.AddCallback(
+    ModCallbacks.MC_PRE_NPC_UPDATE,
+    testDroneUpdate,
+    TESTDRONE_ENTITYTYPE,
+  );
 
   // Print an initialization message to the "log.txt" file
   Isaac.DebugString(`${MOD_NAME} initialized.`);
-}
-
-function postGameStarted() {
-  Isaac.DebugString("Callback triggered: MC_POST_GAME_STARTED");
 }

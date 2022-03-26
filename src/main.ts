6bc58e07b48cef13c09ba83a3c82ee7f1c5f8696
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-26 14:37:15 -0400

    removing superfluous comments

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,8 @@ import {
 const MOD_NAME = "willson-pathfinding";
 
 export function main(): void {
-  // Instantiate a new mod object, which grants the ability to add callback functions that
-  // correspond to in-game events
   const mod = RegisterMod(MOD_NAME, 1);
 
-  // Set a callback function that corresponds to when a new run is started
   mod.AddCallback(ModCallbacks.MC_POST_NEW_ROOM, Pathfinder.rebuildRoom);
   mod.AddCallback(ModCallbacks.MC_POST_UPDATE, Pathfinder.updateRoom);
   mod.AddCallback(ModCallbacks.MC_POST_RENDER, Pathfinder.debugDisplayGrid);
@@ -33,6 +30,5 @@ export function main(): void {
     TESTDRONE_ENTITYTYPE,
   );
 
-  // Print an initialization message to the "log.txt" file
   Isaac.DebugString(`${MOD_NAME} initialized.`);
 }
